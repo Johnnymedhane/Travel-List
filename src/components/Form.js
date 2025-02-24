59a2a291d8d89151bfd37e6fbd55a39669d35dc3
Johnny Medhane <134869490+Johnnymedhane@users.noreply.github.com>
2025-02-24 18:51:18 +0200

    Update Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -17,7 +17,10 @@ export default function Form({onAddItems}) {
     return <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
       <select value={quantity} onChange={(e)=> setQuantity(e.target.value)}>
-        {Array.from({length:20 }, (_, i) => i + 1).map(num => <option value={num} key={num}>{num}</option>)
+      {Array.from({ length: 20 }, (_, i) => i + 1).map(num => (
+  <option value={num} key={num}>{num}</option>
+))}
+
       </select>
       <input type="text" placeholder="item..." value={description} 
       onChange={(e)=> setDescription(e.target.value)}>
