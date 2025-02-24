a176839ffdbd0f12f5ed63f0658b6702577ff5c3
Johnny Medhane <134869490+Johnnymedhane@users.noreply.github.com>
2025-02-24 17:41:00 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,12 @@ import Form from  "./Form"
 import PackingList  from "./PackingList";
 import  State  from "./State";
 
-const initialItems = [
+/*const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "stock", quantity: 12, packed: false},
   { id: 3, description: "charger", quantity: 1, packed: true },
 ];
-
+*/
 
 
 export default function App() {
